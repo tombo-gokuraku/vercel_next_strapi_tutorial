90575534f2d7eb1f98cedf6756f3ff3c8552f39f
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-10-03 10:50:40 +0900

    chore: Apply formatter

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c6d4cd0c136b4d5a0cd09921c1112b92'),
+      secret: env("ADMIN_JWT_SECRET", "c6d4cd0c136b4d5a0cd09921c1112b92"),
     },
   },
 });
